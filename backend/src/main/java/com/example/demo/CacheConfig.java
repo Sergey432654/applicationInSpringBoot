35793adb06dc8a26874199cc67520851a2f99539
Serhii Yemets <115184910+Sergey432654@users.noreply.github.com>
2026-07-23 22:49:58 +0200

    implement redis

--- backend/src/main/java/com/example/demo/CacheConfig.java
+++ backend/src/main/java/com/example/demo/CacheConfig.java
@@ -1,24 +1,33 @@
 package com.example.demo;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
-import java.util.concurrent.TimeUnit;
+import tools.jackson.databind.ObjectMapper;
 
+import java.time.Duration;
 @Configuration
 @EnableCaching
 public class CacheConfig {
+    
 
     @Bean
-    public CacheManager cacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager("cryptoPrices", "marketCoins");
-        cacheManager.setCaffeine(Caffeine.newBuilder()
-                .expireAfterWrite(20, TimeUnit.SECONDS)
-                .maximumSize(200));
-        return cacheManager;
+    public CacheManager cacheManager(RedisConnectionFactory factory) {
+        GenericJacksonJsonRedisSerializer jsonSerializer = new GenericJacksonJsonRedisSerializer(new ObjectMapper());
+        RedisSerializationContext.SerializationPair<Object> valuePair = RedisSerializationContext.SerializationPair.fromSerializer(jsonSerializer);
+
+         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(20))
+                .serializeValuesWith(valuePair);
+        return RedisCacheManager.builder(factory)
+                .cacheDefaults(config)
+                .build();
     }
 }
